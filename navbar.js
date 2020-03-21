56766f82abc08a6fc26ce722d7e5e7d15866b3b6
Alberto <albertoijs@outlook.com>
2020-03-20 22:21:22 -0600

    Fix mobile navbar

--- navbar.js
+++ navbar.js
@@ -2,6 +2,12 @@ var hamburger = document.getElementsByClassName('hamburger')[0];
 var navbar = document.getElementsByClassName('navbar')[0];
 
 function toggleClass() {
+  if (navbar.classList.contains('hidden')) {
+    hamburger.childNodes[1].src = './assets/images/icon-close.svg';
+  } else {
+    hamburger.childNodes[1].src = './assets/images/icon-hamburger.svg';
+  }
+  
   navbar.classList.toggle('hidden');
 }
 
